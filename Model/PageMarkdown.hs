e19df75e3893db6552166c9653fb430a5496c911
haBuu <habuu@habuu.fi>
2018-02-25 19:16:17 +0200

    Show username in version history

--- Model/PageMarkdown.hs
+++ Model/PageMarkdown.hs
@@ -2,6 +2,9 @@ module Model.PageMarkdown where
 
 import Import
 
+import qualified Database.Esqueleto as E
+import Database.Esqueleto((^.))
+
 import Yesod.Markdown
 
 import Model.Page
@@ -10,13 +13,13 @@ getCurrentVersionId :: Page -> DB (Maybe PageMarkdownId)
 getCurrentVersionId page = do
   selectPage page >>= \case
     Just (Entity pid _) -> return $ Just pid
-    Nothing -> return $ Nothing
+    Nothing -> return Nothing
 
 getCurrentVersion :: Page -> DB Int
 getCurrentVersion page = do
   selectPage page >>= \case
     Just (Entity _ pageMarkdown) -> return $ pageMarkdownVersion pageMarkdown
-    Nothing -> return $ 0 -- this should not happen
+    Nothing -> return 0 -- this should not happen
 
 getPageMarkdown :: Page -> DB Markdown
 getPageMarkdown page = do
@@ -24,11 +27,16 @@ getPageMarkdown page = do
     Just (Entity _ pageMarkdown) -> return $ pageMarkdownContent pageMarkdown
     Nothing -> return $ Markdown ""
 
-getPageMarkdownVersions :: Page -> DB [Entity PageMarkdown]
-getPageMarkdownVersions page = selectList
-  [ PageMarkdownPage ==. page
-  , PageMarkdownVersion !=. 0 -- 0 is the empty initial version
-  ] defaultOrder
+getPageMarkdownVersions :: Page -> DB [(Entity PageMarkdown, Entity User)]
+getPageMarkdownVersions page = E.select $
+  E.from $ \(pageMarkdown, user) -> do
+    E.where_ $ pageMarkdown ^. PageMarkdownPage E.==. E.val page
+    E.where_ $ pageMarkdown ^. PageMarkdownUser E.==. user ^. UserId
+    -- 0 is the empty initial version so exclude that
+    E.where_ $ pageMarkdown ^. PageMarkdownVersion E.!=. E.val 0
+    E.orderBy [E.desc (pageMarkdown ^. PageMarkdownVersion)]
+    E.orderBy [E.desc (pageMarkdown ^. PageMarkdownCreated)]
+    return (pageMarkdown, user)
 
 emptyPageMarkdown :: Page -> UTCTime -> UserId -> PageMarkdown
 emptyPageMarkdown page time userId =
