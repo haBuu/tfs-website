a63d4259d3ef5096be5578332ea22bc889710c94
haBuu <habuu@habuu.fi>
2018-03-06 17:04:04 +0200

    Removed Datepicker

--- Datepicker.hs
+++
@@ -1,9 +0,0 @@
-module Datepicker where
-
-import Import
-
-addDatepicker = do
-  addScript $ StaticR js_bootstrap_datepicker_js
-  -- add more languages here (currently finnish and english)
-  addScript $ StaticR js_locales_bootstrap_datepicker_fi_js
-  addStylesheet $ StaticR css_datepicker3_css
